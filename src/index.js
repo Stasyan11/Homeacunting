c7fbca981d4c76756d95e1a1bd3ce2f5b869f408
stasyan11 <you@example.com>
2020-04-08 21:40:52 +0300

    gave a human name for the function and html objects, and also excluded the nested function

--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
-function addDebetLine() {
-  let itm = document.querySelector("#myList1 > div");
-  let cln = itm.cloneNode(true);
-  document.getElementById("myList1").appendChild(cln);
-}
-let button = document.getElementById("button");
+let button = document.getElementById("add-debet-input");
 button.addEventListener("click", addDebetLine);
 
-let button2 = document.getElementById("button2");
+let button2 = document.getElementById("add-credit-input");
 button2.addEventListener("click", addCreditLine);
 
-function addCreditLine() {
-  let itm = document.querySelector("#myList2 > div");
-  let cln = itm.cloneNode(true);
-  document.getElementById("myList2").appendChild(cln);
+function addDebetLine() {
+  const itm = document.querySelector("#Debet-input-area > div");
+  const cln = itm.cloneNode(true);
+  document.getElementById("Debet-input-area").appendChild(cln);
 }
-const reactionONBatton = document.getElementById("add-result");
-reactionONBatton.addEventListener("click", calculateBalance);
-reactionONBatton.addEventListener("click", dispalyBalance);
 
-function calculateBalance() {
+function addCreditLine() {
+  const itm = document.querySelector("#Credit-input-area > div");
+  const cln = itm.cloneNode(true);
+  document.getElementById("Credit-input-area").appendChild(cln);
+}
+let addResult = document.getElementById("add-result");
+addResult.addEventListener("click", calcuteBalance);
+addResult.addEventListener("click", dispalyBalance);
+
+function calcuteBalance() {
   // THIS CODE SUMS UP DEBET-MASIV
-  let arrDebet = [];
-  let addInArrDebet;
+  let masiDebet = [];
+  let addInMasiDebet;
   let inpDebet = document.getElementsByClassName("info-debet");
   for (let i = 0; i < inpDebet.length; i++) {
-    addInArrDebet = Number(inpDebet[i].value);
-    arrDebet.push(addInArrDebet);
+    addInMasiDebet = Number(inpDebet[i].value);
+    masiDebet.push(addInMasiDebet);
   }
   //THIS CODE SUMS UP CREDIT-MASIV
-  const arrCredit = [];
-  let addInArrCredit;
+  let masiCredit = [];
+  let addInMasiCredit;
   let inpCredit = document.getElementsByClassName("info-credit");
   for (let i = 0; i < inpCredit.length; i++) {
-    addInArrCredit = Number(inpCredit[i].value);
-    arrCredit.push(addInArrCredit);
+    addInMasiCredit = Number(inpCredit[i].value);
+    masiCredit.push(addInMasiCredit);
   }
   return {
-    arrDebet: arrDebet,
-    arrCredit: arrCredit,
+    masiDebet: masiDebet,
+    masiCredit: masiCredit,
   };
 }
 
 function dispalyBalance() {
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const resultDebet = calculateBalance().arrDebet.reduce(reducer);
-  const resultCredit = calculateBalance().arrCredit.reduce(reducer);
-
+  let resultDebet = calcuteBalance().masiDebet.reduce(reducer);
+  let resultCredit = calcuteBalance().masiCredit.reduce(reducer);
   document.getElementById("debet-result").innerHTML = resultDebet;
   document.getElementById("debet-result").style.color = "green";
   document.getElementById("credet-result").innerHTML = resultCredit;
